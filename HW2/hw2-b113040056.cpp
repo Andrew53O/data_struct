a20b48747b56e9f50e22000aa9b79035919b54d3
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2023-09-23 15:23:52 +0800

    = operator done

--- HW2/hw2-b113040056.cpp
+++ HW2/hw2-b113040056.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 using namespace std;
 
 // global prototypes
-int countFilledLength(char arr[]);
+int countFilledLength(const char arr[]);
+void sortArrayBasedOnASCII(char arr[]);
 void removeDuplicateCharacter(char arr[]);
 
 class Set 
 {   
     public:
 
+    Set();
+
     // input output operator overloading
     friend istream& operator >>(istream& inputStream, Set& ASet);
 
     // formatted output sorting with ASCII included
     friend ostream& operator <<(ostream& outputStream, Set& ASet);
-
-
+    
+    // using member function copy only arr[256]
+    Set& operator=(const Set& ASet);
+    
     friend const Set operator +(const Set& ASet, const Set& BSet); 
 
 
@@ -55,7 +60,21 @@ int main(void)
         // all operations
         cout << A;
         cout << B;
+        Set C;
 
+        C = A;
+
+        cout << "cout C = " << C;
+    }
+}
+
+
+
+Set::Set()
+{
+    for (int i = 0; i < 256; i++)
+    {
+        arr[i] = '\0';
     }
 }
 
@@ -70,39 +89,45 @@ istream& operator >>(istream& inputStream, Set& ASet)
     inputStream.getline(ASet.arr, sizeof(ASet.arr));
 
     cout << "arrnya " << ASet.arr << endl;
-
+    
+    sortArrayBasedOnASCII(ASet.arr);
+    cout << countFilledLength(ASet.arr) << "??" << strlen(ASet.arr);
     // remove duplicate
     removeDuplicateCharacter(ASet.arr);
+    cout << countFilledLength(ASet.arr) << "??" << strlen(ASet.arr);
 
     return inputStream;
 }
 
 ostream& operator <<(ostream& outputStream, Set& ASet)
 {
-    cout << "arraynya adalah" << ASet.arr << endl;
-
-    cout << sizeof(ASet.arr);
-    // using bubble sort to sort based on ASCII values
+    cout << "{";
+    
     for (int i = 0; i < countFilledLength(ASet.arr); i++)
     {
-        for (int j = i + 1; j < countFilledLength(ASet.arr); j++)
-        {
-            if (ASet.arr[i] > ASet.arr[j])
-            {
-                char temp = ASet.arr[i];
-                ASet.arr[i] = ASet.arr[j];
-                ASet.arr[j] = temp; 
-            }
-        }
-
+        cout << ASet.arr[i]; 
     }
-    ASet.arr[sizeof(ASet.arr) - 1] = '\0';
-    cout << "{" << ASet.arr << "}" << endl;
+    cout << "}";
     return outputStream;
 }
 
 
-int countFilledLength(char arr[])
+Set& Set::operator=(const Set& ASet)
+{
+    int arrayLength = countFilledLength(ASet.arr);
+    cout << "lengthnya adalah" << arrayLength << endl;
+
+    for (int i = 0; i < arrayLength; i++)
+    {
+        this->arr[i] = ASet.arr[i];
+    }
+    this->arr[arrayLength] = '\0';
+
+    return *this;
+}
+
+
+int countFilledLength(const char arr[])
 {
     int length = 0;
 
@@ -114,24 +139,81 @@ int countFilledLength(char arr[])
     return length;
 }
 
-vi
+void sortArrayBasedOnASCII(char arr[])
+{
+    // using bubble sort to sort based on ASCII values
+    for (int i = 0; i < countFilledLength(arr); i++)
+    {
+        for (int j = i + 1; j < countFilledLength(arr); j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp; 
+            }
+        }
+
+    }
+    arr[countFilledLength(arr)] = '\0';
+}
 
 void removeDuplicateCharacter(char arr[])
 {
     int arrayLength = countFilledLength(arr);
 
-    int tempArray[arrayLength];
-
-    for (int i = 0; i < arrayLength - 1; i++)
+    char *tempArray = new char[arrayLength];
+    if (tempArray == NULL)
     {
-        if (arr[])
+        fprintf(stderr, "Memory allocation failed\n");
+        return; // Handle memory allocation failure
     }
 
+
+    // initialize tempArray
+    for (int i = 0; i < arrayLength; i++)
+    {
+        tempArray[i] = '\0';
+    }
+
+    int tempArrayIndex = 0;
+    bool isDuplicate = false;
+
+    for (int i = 0; i < arrayLength; i++)
+    {   
+        isDuplicate = false;
+
+        // because it is sorted we can assign j = i + 1
+        for (int j = i + 1; j < arrayLength - 1; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                isDuplicate = true;
+                break;
+            }   
+        }
+
+        if (isDuplicate == false)
+        {
+            tempArray[tempArrayIndex] = arr[i];
+            tempArrayIndex++;
+        }
+    }
+
+    // copy the temporary to the array
+    for (int i = 0; i < tempArrayIndex; i++)
+    {
+        arr[i] = tempArray[i];
+
+    }
+    // add null terminator
+    arr[tempArrayIndex] = '\0';
+
+    delete[] tempArray;
 }
 
 
 // to do 
-// remove duplicate charater
 // 1. create '=' operator
 // 2. test the + operator
 // 3. continue finish all of the operator
