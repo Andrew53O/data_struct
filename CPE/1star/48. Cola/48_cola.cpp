9017cb95ab96b6ff979b4e8a7e45264d74e9da1d
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2025-10-06 12:50:25 +0800

    add 32. divide solution

--- CPE/1star/48. Cola/48_cola.cpp
+++ CPE/1star/48. Cola/48_cola.cpp
@@ -12,3 +12,43 @@ int main()
 	}
 	return 0;
 }
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, m; 
+	float temp;
+	vector<int> res; 
+	
+	while(cin >> n >> m)
+	{
+		
+		while(n > m)
+		{
+			temp = n / m;
+			
+			res.push_back(temp);
+		}
+		
+		if (temp == 1)
+		{
+			for (int i = 0; i < res.size(); i++)
+			{
+				cout << res[i];
+				if (i != res.size() -1) cout << " ";
+			}
+		}
+		else
+		{
+			cout << "Boring!" << endl;
+		}
+	
+	
+	}
+	
+	return 0;
+}
