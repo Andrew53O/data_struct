d84982f86779946a8a1b7ba41e691c391f227c38
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2025-10-12 23:55:56 +0800

    add 3 primary arithmetic solution

---
+++ CPE/1star/3. primary arithmetic/compactsolution.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int findCarry(int a, int b)
+{
+	int count = 0; 
+	int nA = 0; int nB = 0;
+	while(a!= 0 && b != 0)
+	{
+		nA = a % 10 ;
+		a /= 10;
+		
+		nB = b % 10; 
+		b /= 10;
+		
+		if (nA + nB > 9)
+		{
+			count++; // one carry operation
+			a++; // add one to the front 
+		}
+	}
+	
+	return count;
+
+}
+
+int main(void)
+{
+	int a, b; 
+	while(cin >> a >> b)
+	{
+		if (a == 0 && b == 0)break;
+		
+		int res = findCarry(a,b);
+		
+		if (res == 0 ) cout << "No carry operation." << endl;
+		else if (res == 1) cout << "1 carry operation." << endl;
+		else  cout << res << " carry operations." << endl;
+		 
+	}
+
+	return 0;
+}
